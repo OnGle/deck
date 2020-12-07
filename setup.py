fe7e505327e70635c9f156c7d286836179b38ba5
Stefan Davis <stefan@turnkeylinux.org>
2020-12-07 16:37:27 +1100

    Rewrite deck in python3

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name="deck",
+    version="2.1",
+    author="Stefan Davis",
+    author_email="stefan@turnkeylinux.org",
+    url="https://github.com/turnkeylinux/deck",
+    scripts=["deck"]
+)
